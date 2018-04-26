a2ac8cf896aa01ccc0de28ae32f3851bd7a0a767
Devashish <devashish.86@gmail.com>
2018-04-26 13:38:45 +0530

    adding base packages

--- install.sh
+++ install.sh
@@ -8,15 +8,54 @@ chmod 1777 /tmp
 echo "================= Updating package lists ==================="
 apt-get update
 
+echo "================= Adding some global settings ==================="
+mv gbl_env.sh /etc/profile.d/
+mkdir -p "$HOME/.ssh/"
+mv config "$HOME/.ssh/"
+mv 90forceyes /etc/apt/apt.conf.d/
+touch "$HOME/.ssh/known_hosts"
+mkdir -p /etc/drydock
+
+echo "================= Installing basic packages ==================="
+apt-get install -q -y \
+  build-essential=12.1* \
+  curl=7.47* \
+  gcc=4:5.3* \
+  gettext=0.19* \
+  htop=2.0* \
+  libxml2-dev=2.9* \
+  libxslt1-dev=1.1* \
+  make=4.1* \
+  nano=2.5* \
+  openssh-client=1:7* \
+  openssl=1.0* \
+  software-properties-common=0.96* \
+  sudo=1.8*  \
+  texinfo=6.1* \
+  zip=3.0* \
+  unzip=6.0* \
+  wget=1.17* \
+  rsync=3.1* \
+  psmisc=22.21* \
+  netcat-openbsd=1.105* \
+  vim=2:7.4* \
+  groff=1.22.*
+
 echo "================= Installing core binaries ==================="
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update
 
 apt-get install -yy g++-4.9
 
+echo "================= Installing Python packages ==================="
+apt-get install -q -y \
+  python-pip=8.1* \
+  python-software-properties=0.96* \
+  python-dev=2.7*
+
+pip install -q virtualenv==15.2.0
+pip install -q pyOpenSSL==17.5.0
 rm -rf /usr/local/lib/python2.7/dist-packages/requests*
-pip install --upgrade pip
-hash -r
 
 echo "================== Installing python requirements ====="
 pip install -r /home/shippable/appBase/requirements.txt
